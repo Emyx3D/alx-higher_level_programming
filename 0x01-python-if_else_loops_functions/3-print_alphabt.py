8d798e175df9dea898cf46d1cc2087eef362188d
Emyx3D <emixjasper@gmail.com>
2022-11-29 21:52:44 +0100

    if_else_function

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import string
-for letter in string.ascii_lowercase:
-    if letter not in 'qe':
-        print(letter, end ="")
+for i in range(97, 123):
+    if i == 101 or i == 113:
+        continue
+    print('{}'.format(chr(i)), end="")
